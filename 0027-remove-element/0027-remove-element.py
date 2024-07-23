f70670d0a9192a9ee704f626927fbd089d3debf9
LucaB03 <79718944+LucaB03@users.noreply.github.com>
2024-07-23 22:15:47 +0200

    Time: 32 ms (86.38%), Space: 16.5 MB (65.00%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,11 +1,8 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        nums.sort()
-        try:
-            index = nums.index(val)
-        except ValueError:
-            return len(nums)
-        while index < len(nums) and nums[index] == val:
-            nums.pop(index)
-        return len(nums)
-            
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[index] = nums[i]
+                index += 1
+        return index
